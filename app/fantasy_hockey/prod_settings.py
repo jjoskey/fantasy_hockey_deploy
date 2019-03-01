a3da7b71379edfccc4947f84007cef6fd4b9b341
jjoskey <alexander.s.ilyin@gmail.com>
2019-03-01 13:36:55 +0500

    1. remove admin.css from static 2. add prod_settings

--- app/fantasy_hockey/prod_settings.py
+++ app/fantasy_hockey/prod_settings.py
@@ -1,5 +1,4 @@
-# test settings
-
+# prod settings
 
 import os
 
@@ -109,9 +108,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static') #a
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 LOGIN_REDIRECT_URL = 'loggedin'
 LOGOUT_REDIRECT_URL = 'thanks'
