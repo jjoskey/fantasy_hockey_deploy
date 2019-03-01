de69271565ce7cc13db1dff3531701a71472662e
jjoskey <alexander.s.ilyin@gmail.com>
2019-03-01 12:28:10 +0500

    1

--- app/fantasy_hockey/settings.py
+++ app/fantasy_hockey/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 # STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static') #a
 
 LOGIN_REDIRECT_URL = 'loggedin'
 LOGOUT_REDIRECT_URL = 'thanks'
