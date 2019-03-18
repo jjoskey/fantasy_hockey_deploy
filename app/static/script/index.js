94c2717622f36205842002748e45995288df5f02
jjoskey <alexander.s.ilyin@gmail.com>
2019-03-18 11:13:41 +0500

    fixed bug with invalid date

--- app/static/script/index.js
+++ app/static/script/index.js
@@ -3,7 +3,10 @@
 const timeElements = $('.time');
 //console.log(timeElements);
 timeElements.each(function() {
+    console.log($(this).attr('data-time'));
     const date = new Date($(this).attr('data-time'));
+    console.log(date);
+
 //    console.log(date)
     $(this).text(date.toLocaleString('ru', {day: '2-digit', month: '2-digit' ,hour: '2-digit', minute:'2-digit'}));
 //    console.log($(this).attr('data-time'));
