3ffa567f6dc91cdadd8499235cb29894f5714de8
jjoskey <alexander.s.ilyin@gmail.com>
2019-03-22 16:39:28 +0500

    add row color for leaders

--- app/static/script/leaders.js
+++ app/static/script/leaders.js
@@ -6,4 +6,15 @@
         window.location.href = link;
     })
 
+    $('.user_row').each( function(index, element) {
+        console.log(element, index)
+        if (index === 0) {
+            $(element).css('background-color', 'gold')
+        } else if (index === 1) {
+            $(element).css('background-color', 'silver')
+        } else if (index === 2) {
+            $(element).css('background-color', '#cd7f32')
+        }
+    })
+
 })(window)
